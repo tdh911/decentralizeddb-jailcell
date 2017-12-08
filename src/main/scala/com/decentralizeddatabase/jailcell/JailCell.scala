91f0eaa2519ddeb7116cdda38705a2312029af9c
Tim <tdh911@gmail.com>
2017-12-08 15:33:56 -0600

    Initial scala commit

---
+++ src/main/scala/com/decentralizeddatabase/jailcell/JailCell.scala
@@ -0,0 +1,15 @@
+package com.decentralizeddatabase.jailcell
+
+/**
+ * @author ${user.name}
+ */
+object JailCell {
+  
+  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  
+  def main(args : Array[String]) {
+    println( "Hello World!" )
+    println("concat arguments = " + foo(args))
+  }
+
+}
