7050165284880ef2eb558914832e903af1d555f3
Tim <tdh911@gmail.com>
2018-01-17 16:48:01 -0600

    JailCellRequest and Response classes added

--- src/main/scala/com/decentralizeddatabase/jailcell/JailCellPackets.scala
+++ src/main/scala/com/decentralizeddatabase/jailcell/JailCellPackets.scala
@@ -1,9 +1,5 @@
 package com.decentralizeddatabase.jailcell
 
-import javax.servlet.http.HttpServletRequest
-
 case class JailCellRequest(method: String, keys: List[String], blocks: List[String])
 
-class JailCellResponse {
-//TODO
-}
+case class JailCellResponse(blocks: List[String])
